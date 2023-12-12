91e673aa66f6f007874d66a7dda8b5150192a0d1
Anasch113 <anaschaudry2002@gmail.com>
2023-12-13 00:01:48 +0500

    first commit in build

--- src/pages/MedicalIdentification.jsx
+++ src/pages/MedicalIdentification.jsx
@@ -8,43 +8,18 @@ const MedicalIdVerification = () => {
   const [error, setError] = useState("");
   const { setMedicalId: setValidMedicalId } = useMedicalId();
   const navigate = useNavigate();
-
-  // Function to fetch patient_id
-  const fetchPatientId = async () => {
-    try {
-      if (medicalId) {
-        const response = await fetch(
-          `https://api.pamojapanafrica.com/patient_verification.php?patient_id=${encodeURIComponent(
-            medicalId
-          )}`
-        );
-
-        if (!response.ok) {
-          throw new Error(`Server responded with status ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log("Response data:", data);
-
-        if (data.status === "true") {
-          // Patient is verified, navigate to chat page
-          setValidMedicalId(medicalId);
-          navigate("/chat");
-        } else {
-          // Patient is not verified, show error message
-          setError("Invalid Medical ID. Please try again.");
-        }
-      }
-    } catch (error) {
-      console.error("Error fetching patient_id:", error.message);
-      setError("Failed to fetch patient_id. Please try again.");
-    }
+  const isMedicalIdValid = (id) => {
+    return id === "2023-11-29308";
   };
 
-  // Click event handler for the "Continue" button
+
   const handleContinue = () => {
-    // Call the function to fetch patient_id when the button is clicked
-    fetchPatientId();
+    if (isMedicalIdValid(medicalId)) {
+      setValidMedicalId(medicalId);
+      navigate("/chat");
+    } else {
+      setError("Invalid Medical ID. Please try again.");
+    }
   };
 
   return (
